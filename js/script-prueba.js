b6655a715c299745ab9c2cd619bc271881019e43
victor.bolivar <vjbc94@gmail.com>
2022-03-17 23:26:15 -0300

    Segunda Entrega

--- js/script-prueba.js
+++ js/script-prueba.js
@@ -1,420 +1,238 @@
-
-let nombreArticuloA = "Remera";
-let stockArticuloA = 40;
-let precioArticuloA = "500";
-
-let nombreArticuloB = "Jean";
-let stockArticuloB = 25;
-let precioArticuloB = "1000";
-
-let nombreArticuloC = "Buzo";
-let stockArticuloC = 30;
-let precioArticuloC = "850";
-let metodos = "PagoFacil , MercadoP , Debito, Credito";
-
-
-
-class21
-
-
-
-
-
-
-
-let nombreUsurio = prompt("Hola nos prodrias indicar tu nombre");{
-    console.log("Usuario " + nombreUsurio + " se registro")
-}
-
-let cantidadProductosComprados = parseInt(prompt("Cuantos productos diferentes quieres comprar: \n - Remera \n - Jean \n - Buzo"))
-let cantidadComprada;
+let cantidadComprada = 0;
 let precioTotalVenta = 0;
 
 
-for( let i = 0 ; i < cantidadProductosComprados; i++ ){
-    let nombreCompra = prompt("Ingrese el nombre del articulo que desea comprar (Elegi primero los nombres y luego la cantidad)")
-
-if ( nombreCompra == nombreArticuloA) {
-    cantidadComprada = parseInt (prompt("Ingrese la cantidad que quiere comprar"));
-    if (cantidadComprada <= stockArticuloA){
-        stockArticuloA = stockArticuloA - cantidadComprada;
-        precioTotalVenta += cantidadComprada * precioArticuloA;
-        console.log(nombreUsurio + " realizo la compra de " + cantidadComprada + ' ' + nombreCompra + 's');
-        console.log("Quedan " + stockArticuloA + " remeras en stock");
-    }
-    else {
-        alert ("Solo contamos con esta cantidad " + stockArticuloA + " unidades")
-    }
-}
-
-if ( nombreCompra == nombreArticuloB) {
-    cantidadComprada = parseInt (prompt("Ingrese la cantidad que quiere comprar"));
-    if (cantidadComprada <= stockArticuloB){
-        stockArticuloB = stockArticuloB - cantidadComprada;
-        precioTotalVenta += cantidadComprada * precioArticuloB;
-        console.log(nombreUsurio + " realizo la compra de " + cantidadComprada + ' ' + nombreCompra + 's');
-        console.log("Quedan " + stockArticuloB + " jean en stock");
-    }
-    else {
-        alert ("Solo contamos con esta cantidad " + stockArticuloB + " unidades")
+/* Estructura de Articulos */
+class Articulo {
+    constructor(nombre, stock, precio, imagenes, categoria, id) {
+        this.nombre = nombre;
+        this.stock = stock;
+        this.precio = precio;
+        this.imagenes = imagenes;
+        this.categoria = categoria;
+        this.id = id;
+        this.venta = function (cantidadComprada) {
+            this.stock -= cantidadComprada;
+            console.log("El stock remanente es de: " + this.stock + " " + this.nombre);
+        };
     }
 }
 
-if ( nombreCompra == nombreArticuloC) {
-    cantidadComprada = parseInt (prompt("Ingrese la cantidad que quiere comprar"));
-    if (cantidadComprada <= stockArticuloC){
-        stockArticuloC = stockArticuloC - cantidadComprada;
-        precioTotalVenta += cantidadComprada * precioArticuloC;
-        console.log(nombreUsurio + " realizo la compra de " + cantidadComprada + ' ' + nombreCompra + 's');
-        console.log("Quedan " + stockArticuloC + " jean en stock");
-    }
-    else {
-        alert ("Solo contamos con esta cantidad " + stockArticuloC + " unidades")
-    }
-}
+/* Articuloos Tienda */
 
-}
-
-let entrada = prompt("Indicar 'Finalizar' para terminar la compra");
-while(entrada != "Finalizar" ){
-    alert("El usuario ingresó "+ entrada);
-    entrada = prompt("Ingresar 'Finalizar' para terminar la operacion. Y realizar el pago");
-}
-
-console.log("Total venta realiazada a usuario " + nombreUsurio + ' por $' + precioTotalVenta  )
-
-alert("Muchas Gracias por elegirnos " + nombreUsurio +  ", El precio de su compra es " + '$' + precioTotalVenta);
-
-
-/* Arrays & Objetos */
-
-function Envios (nombreValor, precioValor){
-    this.nombre = nombreValor;
-    this.precio = precioValor;
-}
-
-let contador = 0
-let listadoEnvios = "Enviamos con estas empresas: "
-
-const EnviosA = new Envios("MercadoFlex", 450)
-const EnviosB = new Envios("PeYa", 450)
-
-
-const listaEnvios = [EnviosA, EnviosB]
-
-for(const envios of listaEnvios){
-    contador++
-    listadoEnvios += "\n" + contador + "- " + envios.nombre + " $" + envios.precio
-    console.log("envio mediante " + envios.nombre)
-}
-
-
-function listarEnvios(){
-    alert(listadoEnvios)
-}
-
-
-function metodo(){
-    let opcion = prompt("Seleccione metodo de pago: \n1 - Efectivo \n2 - Debito/Credito \n3- MercadoP")
-
-    switch(opcion){
-        case "1":
-            efectivo()
-            listarEnvios();
-            break;
-        case "2":
-            debito();
-            listarEnvios();
-            break;
-        case "3":
-            MercadoP()
-            listarEnvios();
-            break;
-        default:
-            alert("Eliga una opcion correcta")
-            metodo()
-            break;
-    }
-}
-
-function efectivo(){
-    prompt("Realizar el pago mediante: \n1- PagoFacil \n2- RapiPago")
-    alert("Gracias por su compra, pago realizado con exito")
-    console.log("Se acredito el pago de " + nombreUsurio + " por $" + precioTotalVenta)
-}
-
-function debito(){
-    prompt("Seleccionar tipo de tarjeta: \n1- Debito \n2- Credito")
-    alert("Gracias por su compra, pago realizado con exito")
-    console.log("Se acredito el pago de " + nombreUsurio + " por $" + precioTotalVenta)
-}
-
-function MercadoP(){
-    prompt("Le enviaremos un link de pago via Whatsapp, indique su numero")
-    alert("Gracias por su compra, pago realizado con exito")
-    console.log("Se acredito el pago de " + nombreUsurio + " por $" + precioTotalVenta)
-}
-
-
-
-metodo()
-
-/* Arrays & Objetos */
-
-const Direccion = [];
-let   cantidad     = 1;
-do{
-   let entrada = prompt("Ingresar Direccion de entrega");
-    console.log ("Direccion a enviar " + entrada);
-   Direccion.push(entrada.toUpperCase());
-}while(Direccion.length != cantidad)
-const compraFinalizada = Direccion.concat(["Sera enviado en el transcuro de 2 hs"]);
-alert(compraFinalizada.join("\n"));
-
-
-
-/* l
-for (const articulo of Articulos) {
-    ArticulosMenu += "\n" + "- " + articulo.nombre
-}
- */
-
-
-
-/* Envios */
-
-/* function Envios(nombreValor, precioValor) {
-    this.nombre = nombreValor;
-    this.precio = precioValor;
-}
-
-let contador = 0
-let listadoEnvios = "Enviamos con estas empresas: "
-
-const EnviosA = new Envios("MercadoFlex", 450)
-const EnviosB = new Envios("PeYa", 450)
-const EnviosC = new Envios("Retiro en tiendo", 0)
-
-
-const listaEnvios = [EnviosA, EnviosB, EnviosC]
-
-for (const envios of listaEnvios) {
-    contador++
-    listadoEnvios += "\n" + contador + "- " + envios.nombre + " $" + envios.precio
-} */
-
-/* Nombre usuario */
-/* 
-let nombreUsurio = prompt("Hola nos prodrias indicar tu nombre y apellido");{
-    console.log("Usuario " + nombreUsurio + " se registro")
-} */
-
-/* Metodo de pago */
-/* function metodo(){
-    let opcion = prompt("Seleccione metodo de pago: \n1 - Efectivo \n2 - Debito/Credito \n3- MercadoPago")
-
-    switch(opcion){
-        case "1":
-            efectivo();
-            break;
-        case "2":
-            debito();
-            break;
-        case "3":
-            MercadoP()
-            break;
-        default:
-            alert("Eliga una opcion correcta")
-            metodo()
-            break;
-    }
-}
- */
-/* Menu de compra */
-
-/* function menu(){
-    let opcion = prompt("Opciones: \n1 - Ver productos \n2 - Whatsapp \n3 - ShowRoom");
-
-    switch(opcion){
-        case "1":
-            listaArticulos();
-            comprarArticulos();
-            metodo();
-            listarEnvios();
-            break;
-        case "2":
-            whatsapp();
-            break;
-        case "3":
-            ShowRoom();
-            break;
-        default:
-            alert("Opcion Incorrecta")
-            break;
-    }
-}
- */
-
-
-/* 
-let cantidadComprada;
-let precioTotalVenta = 0; */
-
-
-
-/* Funciones de menu  */
+const articuloA = new Articulo(
+    "Remera",
+    50,
+    500,
+    "./imagenes/remera.jpg",
+    "remera",
+    1
+);
 
 
 
 
-/* function listaArticulos() {
-    alert(ArticulosMenu);
-}
+const articuloB = new Articulo(
+    "Jean",
+    50,
+    800,
+    "./imagenes/jean.jpg",
+    "jean",
+    2,
+);
 
 
-function stockInsuficiente(stock) {
-    alert("Solo contamos con esta cantidad " + stock + " unidades")
-}
 
-function stockSuficiente(stock) {
-    stock = stock - cantidadComprada
-}
+const articuloC = new Articulo(
+    "Buzo",
+    50,
+    750,
+    "./imagenes/buzo.jpg",
+    "buzo",
+    3)
 
-function stockRestante(stock, articulo) {
-    stock = stock - cantidadComprada + ' ' + articulo.nombre
-    console.log("Quedan la siguiente cantidadd: " + stock);
-}
 
-function registroVenta(articulo) {
-    console.log(nombreUsurio + " realizo la compra de " + cantidadComprada + ' ' + articulo.nombre + 's');
-}
+const articuloD = new Articulo(
+    "Vestido",
+    50,
+    830,
+    "./imagenes/vestido.jpg",
+    "vestido",
+    4
+)
 
-function calcularPrecio(precio) {
-    precioTotalVenta += cantidadComprada * precio;
-}
 
-function whatsapp() {
-    alert("+54 9 11 xxxxx")
-}
+const articuloE = new Articulo(
+    "Campera",
+    50,
+    1500,
+    "./imagenes/campera.jpg",
+    "campera",
+    5
+)
 
-function ShowRoom() {
-    alert("Nos encontramos atendiedo en el horario de 8:00am-12:00pm")
-    prompt("Indicar la hora para agendar cita")
-}
 
-function compra(stock, precio, articulo) {
-    cantidadComprada = prompt("Cuanto productos quiere comprar");
-    if (cantidadComprada <= stock) {
-        articulo.venta(cantidadComprada);
-        if (cantidadComprada < 50) {
-            calcularPrecio(precio);
-            stockRestante(stock, articulo);
-            registroVenta(articulo)
-        }
+const articuloF = new Articulo(
+    "Remera",
+    50,
+    500,
+    "./imagenes/remera.jpg",
+    "remera",
+    6
+)
 
-    }
-    else {
-        stockInsuficiente(stock)
+
+const articuloG = new Articulo(
+    "Jean",
+    50,
+    800,
+    "./imagenes/jean.jpg",
+    "jean",
+    7
+)
+
+
+const articuloH = new Articulo(
+    "Buzo",
+    50,
+    750,
+    "./imagenes/buzo.jpg",
+    "buzo",
+    8
+)
+
+
+const articuloI = new Articulo(
+    "Vestido",
+    50,
+    830,
+    "./imagenes/vestido.jpg",
+    "vestido",
+    9
+)
+
+
+const articuloJ = new Articulo(
+    "Campera",
+    50,
+    1500,
+    "./imagenes/campera.jpg",
+    "campera",
+    10
+)
+
+const Articulos = [articuloA, articuloB, articuloC, articuloD, articuloE, articuloF, articuloG, articuloH, articuloI, articuloJ];
+const carrito = [];
+
+
+/* Clase Articulos por categoria */
+
+
+let remera = document.querySelector('.remeras')
+let jean = document.querySelector('.jeans')
+let buzo = document.querySelector('.buzos')
+let vestido = document.querySelector('.vestidos')
+let campera = document.querySelector('.camperas')
+
+
+/* Articulos mostrados en pantalla */
+
+function mostrarProductos() {
+    let ArticulosPantalla = document.querySelector(".ArticulosPantalla");
+
+    ArticulosPantalla.innerHTML = ''
+
+    for (const Articulo of Articulos) {
+        let contenedor = document.createElement("div");
+
+        contenedor.innerHTML = `<div class="card-articulo border">
+                            <img src=${Articulo.imagenes} class="imagen"/>
+                            <h3 id= "nombre">  ${Articulo.nombre}</h3>
+                            <b>$${Articulo.precio}</b>
+                            <p>Disponible: ${Articulo.stock} unidades</p>
+                            <button id="boton${Articulo.id}" type="button" class= "btn btn-primary" onclick="addToCart(${Articulo.precio},${Articulo.id},${Articulo.stock})"> COMPRAR </button>
+                            </div>`
+        ArticulosPantalla.appendChild(contenedor);
     }
 }
 
-function listarEnvios() {
-    alert(listadoEnvios)
+/* Seleccion de articulos por categoria */
+
+remera.addEventListener('click', function () { renderProductos("remera") })
+jean.addEventListener('click', function () { renderProductos("jean") })
+vestido.addEventListener('click', function () { renderProductos("vestido") })
+campera.addEventListener('click', function () { renderProductos("campera") })
+buzo.addEventListener('click', function () { renderProductos("buzo") })
+
+
+/* Articulos por categoria */
+function renderProductos(categoria) {
+    let ArticulosPantalla = document.querySelector('.ArticulosPantalla')
+
+    ArticulosPantalla.innerHTML = ''
+
+    const productos = Articulos.filter(x => x.categoria == categoria)
+    for (const Articulo of productos) {
+        let contenedor = document.createElement("div");
+
+        contenedor.className = "card col-lg-3";
+        contenedor.id = Articulo.id;
+        contenedor.innerHTML = `
+                            <img src=${Articulo.imagenes} class="imagen"/>
+                            <h3 id= "nombre">  ${Articulo.nombre}</h3>
+                            <b>$${Articulo.precio}</b>
+                            <p>Quedan: ${Articulo.stock} unidades</p>
+                            <button id="boton${Articulo.id}" type="button" class= "btn btn-primary" onclick="addToCart(${Articulo.precio},${Articulo.id},${Articulo.stock})"> COMPRAR </button>
+                                `
+        ArticulosPantalla.appendChild(contenedor);
+    }
 }
 
-function comprarArticulos() {
+/* Funcion Agregar carrito */
 
-    let ArticulosComprados = parseInt(prompt("Cuanto productos diferentes quieres comprar"))
+function addToCart(precio, id, stock) {
+    for (const Articulo of Articulos) {
+        if (Articulo.id === id) {
+            if (stock > 0) {
+                carrito.push(precio);
+                Articulo.venta(1);
 
-    for (let i = 0; i < ArticulosComprados; i++) {
-
-        let nombreArticulo = prompt("Que productos desea comprar:")
-
-        if (nombreArticulo == articuloA.nombre) {
-            compra(articuloA.stock, articuloA.precio, articuloA)
-        }
-        else if (nombreArticulo == articuloB.nombre) {
-            compra(articuloB.stock, articuloB.precio, articuloB)
-        }
-        else if (nombreArticulo == articuloB.nombre) {
-            compra(articuloB.stock, articuloB.precio, articuloB)
-        }
-        else if (nombreArticulo == articuloC.nombre) {
-            compra(articuloC.stock, articuloC.precio, articuloC)
-        }
-        else if (nombreArticulo == articuloD.nombre) {
-            compra(articuloD.stock, articuloD.precio, articuloD)
-        }
-        else {
-            alert('No contamos con ese producto, pronto ingresara')
+                precioTotalVenta = carrito.reduce((partialSum, a) => partialSum + a, 0);
+                impresorPrecios();
+                mostrarProductos();
+            } else {
+                stockInsuficiente(Articulo);
+            }
         }
     }
-
-    alert("El precio de su compra es de: $" + precioTotalVenta);
-} */
-
-
-
-
-
-
-/* function efectivo() {
-    prompt("Realizar el pago mediante: \n1- PagoFacil \n2- RapiPago")
-    alert("Gracias por su compra, pago realizado con exito")
-    console.log("Se acredito el pago de " + nombreUsurio + " por $" + precioTotalVenta)
 }
 
-function debito() {
-    prompt("Seleccionar tipo de tarjeta: \n1- Debito \n2- Credito")
-    alert("Gracias por su compra, pago realizado con exito")
-    console.log("Se acredito el pago de " + nombreUsurio + " por $" + precioTotalVenta)
+/* Funcion Stock Insuficiente */
+
+function stockInsuficiente(Articulo) {
+    let inputPrecio = document.querySelector(".total-2");
+    inputPrecio.innerHTML = "";
+
+    let contenedor = document.createElement("div");
+
+    contenedor.innerHTML = `<div class="elTotal-2"">
+                            <b>Stock Agotado de ${Articulo.nombre}</b>`;
+    inputPrecio.appendChild(contenedor);
 }
 
-function MercadoP() {
-    prompt("Le enviaremos un link de pago via Whatsapp, indique su numero")
-    alert("Gracias por su compra, pago realizado con exito")
-    console.log("Se acredito el pago de " + nombreUsurio + " por $" + precioTotalVenta)
-} */
+/* Total de la venta */
+function impresorPrecios() {
+    let inputPrecio = document.querySelector(".total");
+    inputPrecio.innerHTML = "";
 
+    let contenedor = document.createElement("div");
 
-
-
-
-/* 
-menu() */
-
-
-
-
-
-/* Metodo de entrega */
-
-
-
-
-
-/* const Direccion = [];
-let   cantidad     = 1;
-do{
-   let entrada = prompt("Ingresar Direccion de entrega o Retiro en tienda");
-    console.log ("Entregar en " + entrada);
-   Direccion.push(entrada.toUpperCase());
-}while(Direccion.length != cantidad)
-const compraFinalizada = Direccion.concat(["En el transcurso de 2hs tu pedido estara listo para ser enviado o retirado"]);
-alert(compraFinalizada.join("\n"));
- */
-
-
-
-/* Finalizar compra */
-
-
-
-
-/* let entrada = prompt("Indicar 'Finalizar' para terminar la compra");
-while(entrada != "Finalizar" ){
-    alert("El usuario ingresó "+ entrada);
-    entrada = prompt("Ingresar 'Finalizar' para terminar la operacion. Y realizar el pago");
+    contenedor.innerHTML = `<div class="elTotal"">
+                            <b>El total de su boleta es de: $${precioTotalVenta}</b>`;
+    inputPrecio.appendChild(contenedor);
 }
 
-console.log("Total venta realiazada a usuario " + nombreUsurio + ' por $' + precioTotalVenta  ) */
+mostrarProductos();
+impresorPrecios();
+
+console.log(carrito);
--- js/script.js
+++ js/script.js
@@ -1,42 +1,131 @@
-/* Articulos */
+let cantidadComprada = 0;
+let precioTotalVenta = 0;
 
+
+/* Estructura de Articulos */
 class Articulo {
-    constructor(nombre, stock, precio, imagenes, categoria) {
+    constructor(nombre, stock, precio, imagenes, categoria, id) {
         this.nombre = nombre;
         this.stock = stock;
         this.precio = precio;
         this.imagenes = imagenes;
         this.categoria = categoria;
+        this.id = id;
         this.venta = function (cantidadComprada) {
-            this.stock -= cantidadComprada
-        }
+            this.stock -= cantidadComprada;
+            console.log("El stock remanente es de: " + this.stock + " " + this.nombre);
+        };
     }
-
 }
 
-let ArticulosMenu = "Productos en tienda"
+/* Articuloos Tienda */
+
+const articuloA = new Articulo(
+    "Remera",
+    50,
+    500,
+    "./imagenes/remera.jpg",
+    "remera",
+    1
+);
 
 
-const articuloA = new Articulo("Remera", 50, 500, "./imagenes/remera.jpg", "remera")
-const articuloB = new Articulo("Jean", 50, 800, "./imagenes/jean.jpg", "jean")
-const articuloC = new Articulo("Buzo", 50, 750, "./imagenes/buzo.jpg", "buzo")
-const articuloD = new Articulo("Vestido", 50, 830, "./imagenes/vestido.jpg", "vestido")
-const articuloE = new Articulo("Campera", 50, 1500, "./imagenes/campera.jpg", "campera")
-const articuloF = new Articulo("Remera", 50, 500, "./imagenes/remera.jpg", "remera")
-const articuloG = new Articulo("Jean", 50, 800, "./imagenes/jean.jpg", "jean")
-const articuloH = new Articulo("Buzo", 50, 750, "./imagenes/buzo.jpg", "buzo")
-const articuloI = new Articulo("Vestido", 50, 830, "./imagenes/vestido.jpg", "vestido")
-const articuloJ = new Articulo("Campera", 50, 1500, "./imagenes/campera.jpg", "campera")
 
+
+const articuloB = new Articulo(
+    "Jean",
+    50,
+    800,
+    "./imagenes/jean.jpg",
+    "jean",
+    2,
+);
+
+
+
+const articuloC = new Articulo(
+    "Buzo",
+    50,
+    750,
+    "./imagenes/buzo.jpg",
+    "buzo",
+    3)
+
+
+const articuloD = new Articulo(
+    "Vestido",
+    50,
+    830,
+    "./imagenes/vestido.jpg",
+    "vestido",
+    4
+)
+
+
+const articuloE = new Articulo(
+    "Campera",
+    50,
+    1500,
+    "./imagenes/campera.jpg",
+    "campera",
+    5
+)
+
+
+const articuloF = new Articulo(
+    "Remera",
+    50,
+    500,
+    "./imagenes/remera.jpg",
+    "remera",
+    6
+)
+
+
+const articuloG = new Articulo(
+    "Jean",
+    50,
+    800,
+    "./imagenes/jean.jpg",
+    "jean",
+    7
+)
+
+
+const articuloH = new Articulo(
+    "Buzo",
+    50,
+    750,
+    "./imagenes/buzo.jpg",
+    "buzo",
+    8
+)
+
+
+const articuloI = new Articulo(
+    "Vestido",
+    50,
+    830,
+    "./imagenes/vestido.jpg",
+    "vestido",
+    9
+)
+
+
+const articuloJ = new Articulo(
+    "Campera",
+    50,
+    1500,
+    "./imagenes/campera.jpg",
+    "campera",
+    10
+)
 
 const Articulos = [articuloA, articuloB, articuloC, articuloD, articuloE, articuloF, articuloG, articuloH, articuloI, articuloJ];
-const menorPrecio = Articulos.filter(Articulo => Articulo.precio <= 750);
-const mayorPrecio = Articulos.filter(Articulo => Articulo.precio >= 790);
-console.log(menorPrecio);
-console.log(mayorPrecio);
+const carrito = [];
 
 
-/* lista filtro */
+/* Clase Articulos por categoria */
 
 
 let remera = document.querySelector('.remeras')
@@ -45,36 +134,29 @@ let buzo = document.querySelector('.buzos')
 let vestido = document.querySelector('.vestidos')
 let campera = document.querySelector('.camperas')
 
-/* Productos menu */
 
-let ArticulosPantalla = document.querySelector('.ArticulosPantalla')
+/* Articulos mostrados en pantalla */
 
-ArticulosPantalla.innerHTML = ''
+function mostrarProductos() {
+    let ArticulosPantalla = document.querySelector(".ArticulosPantalla");
 
-for (const Articulo of Articulos) {
-    let contenedor = document.createElement("div");
-    contenedor.innerHTML = `<div class="card-articulo border">
+    ArticulosPantalla.innerHTML = ''
+
+    for (const Articulo of Articulos) {
+        let contenedor = document.createElement("div");
+
+        contenedor.innerHTML = `<div class="card-articulo border">
                             <img src=${Articulo.imagenes} class="imagen"/>
                             <h3 id= "nombre">  ${Articulo.nombre}</h3>
-                            <h5 class="text-warning bg-dark"> ${Articulo.precio}</h5>
-                            <input type="number" min="1" max="50" value="1" name="" id="" class="cantidad">
-                            <button type="button" class="btnn" id="2"> COMPRAR </button>
+                            <b>$${Articulo.precio}</b>
+                            <p>Disponible: ${Articulo.stock} unidades</p>
+                            <button id="boton${Articulo.id}" type="button" class= "btn btn-primary" onclick="addToCart(${Articulo.precio},${Articulo.id},${Articulo.stock})"> COMPRAR </button>
                             </div>`
-    ArticulosPantalla.appendChild(contenedor);
-
+        ArticulosPantalla.appendChild(contenedor);
+    }
 }
 
-/* Boton carrito (Falta añadir a todos los botones ) */
-
-let boton = document.getElementById('2')
-boton.addEventListener('click', comprar)
-
-function comprar() {
-    alert('articulo agregado')
-}
-
-
-/* Filtro productos */
+/* Seleccion de articulos por categoria */
 
 remera.addEventListener('click', function () { renderProductos("remera") })
 jean.addEventListener('click', function () { renderProductos("jean") })
@@ -83,7 +165,7 @@ campera.addEventListener('click', function () { renderProductos("campera") })
 buzo.addEventListener('click', function () { renderProductos("buzo") })
 
 
-
+/* Articulos por categoria */
 function renderProductos(categoria) {
     let ArticulosPantalla = document.querySelector('.ArticulosPantalla')
 
@@ -91,15 +173,66 @@ function renderProductos(categoria) {
 
     const productos = Articulos.filter(x => x.categoria == categoria)
     for (const Articulo of productos) {
-
         let contenedor = document.createElement("div");
-        contenedor.innerHTML = `<div class="card-articulo border">
-                                <img src=${Articulo.imagenes} class="imagen"/>
-                                <h3 id= "nombre">  ${Articulo.nombre}</h3>
-                                <h5 class="text-warning bg-dark"> ${Articulo.precio}</h5>
-                                <input type="number" min="1" max="50" value="1" name="" id="" class="cantidad">
-                                <button type="button" class="btnn" id="2"> COMPRAR </button>
-                                </div>`
+
+        contenedor.className = "card col-lg-3";
+        contenedor.id = Articulo.id;
+        contenedor.innerHTML = `
+                            <img src=${Articulo.imagenes} class="imagen"/>
+                            <h3 id= "nombre">  ${Articulo.nombre}</h3>
+                            <b>$${Articulo.precio}</b>
+                            <p>Quedan: ${Articulo.stock} unidades</p>
+                            <button id="boton${Articulo.id}" type="button" class= "btn btn-primary" onclick="addToCart(${Articulo.precio},${Articulo.id},${Articulo.stock})"> COMPRAR </button>
+                                `
         ArticulosPantalla.appendChild(contenedor);
     }
 }
+
+/* Funcion Agregar carrito */
+
+function addToCart(precio, id, stock) {
+    for (const Articulo of Articulos) {
+        if (Articulo.id === id) {
+            if (stock > 0) {
+                carrito.push(precio);
+                Articulo.venta(1);
+
+                precioTotalVenta = carrito.reduce((partialSum, a) => partialSum + a, 0);
+                impresorPrecios();
+                mostrarProductos();
+            } else {
+                stockInsuficiente(Articulo);
+            }
+        }
+    }
+}
+
+/* Funcion Stock Insuficiente */
+
+function stockInsuficiente(Articulo) {
+    let inputPrecio = document.querySelector(".total-2");
+    inputPrecio.innerHTML = "";
+
+    let contenedor = document.createElement("div");
+
+    contenedor.innerHTML = `<div class="elTotal-2"">
+                            <b>Stock Agotado de ${Articulo.nombre}</b>`;
+    inputPrecio.appendChild(contenedor);
+}
+
+/* Total de la venta */
+function PrecioPantalla() {
+    let inputPrecio = document.querySelector(".total");
+    inputPrecio.innerHTML = "";
+
+    let contenedor = document.createElement("div");
+
+    contenedor.innerHTML = `<div class="elTotal"">
+                            <b>Total agregado: $${precioTotalVenta}</b>`;
+    inputPrecio.appendChild(contenedor);
+}
+
+mostrarProductos();
+PrecioPantalla();
+
+console.log(carrito);
