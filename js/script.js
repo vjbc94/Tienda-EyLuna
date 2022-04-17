d1ecea873f660175a48e84d69ab775b0be657720
victor.bolivar <vjbc94@gmail.com>
2022-04-17 20:54:14 -0300

    Proyecto Final

--- js/script.js
+++ js/script.js
@@ -2,152 +2,44 @@ let cantidadComprada = 0;
 let precioTotalVenta = 0;
 let listaProductos = [];
 
-/* Estructura de Articulos */
-function Producto (nombre, stock, precio, imagenes, categoria, id) {
-        this.nombre = nombre;
-        this.stock = stock;
-        this.precio = precio;
-        this.imagenes = imagenes;
-        this.categoria = categoria;
-        this.id = id;
-        this.venta = function (cantidadComprada) {
-            this.stock -= cantidadComprada;
-            console.log("El stock remanente es de: " + this.stock + " " + this.nombre);
-        };
-    }
+/* Estructura de Productos */
+function Producto(nombre, stock, precio, imagenes, categoria, id) {
+    this.nombre = nombre;
+    this.stock = stock;
+    this.precio = precio;
+    this.imagenes = imagenes;
+    this.categoria = categoria;
+    this.id = id;
+    this.venta = function (cantidadComprada) {
+        this.stock -= cantidadComprada;
+        console.log("El stock remanente es de: " + this.stock + " " + this.nombre);
+    };
+}
 
-    fetch("productos.json")
-	.then(response => response.json())
-	.then(data => {
-		data.forEach(producto =>
-			listaProductos.push(
-				new Producto(
-					producto.nombre,
+/* Productos Cargados del Json */
+
+fetch("productos.json")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(producto =>
+            listaProductos.push(
+                new Producto(
+                    producto.nombre,
                     producto.stock,
-					producto.precio,
+                    producto.precio,
                     producto.imagenes,
-					producto.categoria,
-					producto.id,
-				)
-			)
-		);
-		mostrarProductos();
-		console.log(listaProductos);
-	});
+                    producto.categoria,
+                    producto.id,
+                )
+            )
+        );
+        mostrarProductos();
+    });
 
-
-
-
-
-/* Articuloos Tienda */
-
-/* const articuloA = new Articulo(
-    "Remera",
-    50,
-    500,
-    "./imagenes/remera.jpg",
-    "remera",
-    1
-);
-
-
-
-
-const articuloB = new Articulo(
-    "Jean",
-    50,
-    800,
-    "./imagenes/jean.jpg",
-    "jean",
-    2,
-);
-
-
-
-const articuloC = new Articulo(
-    "Buzo",
-    50,
-    750,
-    "./imagenes/buzo.jpg",
-    "buzo",
-    3)
-
-
-const articuloD = new Articulo(
-    "Vestido",
-    50,
-    830,
-    "./imagenes/vestido.jpg",
-    "vestido",
-    4
-)
-
-
-const articuloE = new Articulo(
-    "Campera",
-    50,
-    1500,
-    "./imagenes/campera.jpg",
-    "campera",
-    5
-)
-
-
-const articuloF = new Articulo(
-    "Remera",
-    50,
-    500,
-    "./imagenes/remera.jpg",
-    "remera",
-    6
-)
-
-
-const articuloG = new Articulo(
-    "Jean",
-    50,
-    800,
-    "./imagenes/jean.jpg",
-    "jean",
-    7
-)
-
-
-const articuloH = new Articulo(
-    "Buzo",
-    50,
-    750,
-    "./imagenes/buzo.jpg",
-    "buzo",
-    8
-)
-
-
-const articuloI = new Articulo(
-    "Vestido",
-    50,
-    830,
-    "./imagenes/vestido.jpg",
-    "vestido",
-    9
-)
-
-
-const articuloJ = new Articulo(
-    "Campera",
-    50,
-    1500,
-    "./imagenes/campera.jpg",
-    "campera",
-    10
-)
-
-const Articulos = [articuloA, articuloB, articuloC, articuloD, articuloE, articuloF, articuloG, articuloH, articuloI, articuloJ]; */
 const carrito = [];
 
 
-/* Clase Articulos por categoria */
-
+/* Clase Productos por categoria */
 
 let remera = document.querySelector('.remeras')
 let jean = document.querySelector('.jeans')
@@ -156,21 +48,18 @@ let vestido = document.querySelector('.vestidos')
 let campera = document.querySelector('.camperas')
 
 
-/* Articulos mostrados en pantalla */
+/* Productos mostrados en pantalla */
 
 function mostrarProductos() {
     let ArticulosPantalla = document.querySelector(".ArticulosPantalla");
-
     ArticulosPantalla.innerHTML = ''
 
     for (const producto of listaProductos) {
         let nombre = producto.nombre;
         let imagenes = producto.imagenes;
-        let precio =  producto.precio;
+        let precio = producto.precio;
         let id = producto.id;
         let stock = producto.stock;
-
-
         let contenedor = document.createElement("div");
 
         contenedor.innerHTML = `<div class="card-articulo border">
@@ -178,13 +67,13 @@ function mostrarProductos() {
                             <h3 id= "nombre">  ${nombre}</h3>
                             <b>$${precio}</b>
                             <p>Disponible: ${stock} unidades</p>
-                            <button id="boton${id}" type="button" class= "btn btn-outline-dark" onclick="addToCart(${precio},${id},${stock})"> COMPRAR </button>
+                            <button id="boton${id}" type="button" class= "btn btn-outline-dark" onclick="card(${precio},${id},${stock})"> COMPRAR </button>
                             </div>`
         ArticulosPantalla.appendChild(contenedor);
     }
 }
 
-/* Seleccion de articulos por categoria */
+/* Seleccion de productos por categoria */
 
 remera.addEventListener('click', function () { renderProductos("remera") })
 jean.addEventListener('click', function () { renderProductos("jean") })
@@ -193,28 +82,25 @@ campera.addEventListener('click', function () { renderProductos("campera") })
 buzo.addEventListener('click', function () { renderProductos("buzo") })
 
 
-/* Articulos por categoria */
+/* Productos por categoria */
 function renderProductos(categoria) {
     let ArticulosPantalla = document.querySelector('.ArticulosPantalla')
     ArticulosPantalla.innerHTML = ''
-    const productos = Articulos.filter(x => x.categoria == categoria)
-    for (const Articulo of productos) {
+    const productos = listaProductos.filter(x => x.categoria == categoria)
+    for (const producto of productos) {
         /* Agregado desestructuracion */
-        let nombre = Articulo.nombre;
-        let imagenes = Articulo.imagenes;
-        let precio =  Articulo.precio;
-        let id = Articulo.id;
-        let stock = Articulo.stock;
-
-
+        let nombre = producto.nombre;
+        let imagenes = producto.imagenes;
+        let precio = producto.precio;
+        let id = producto.id;
+        let stock = producto.stock;
         let contenedor = document.createElement("div");
-
         contenedor.innerHTML = `<div class="card-articulo border">
                             <img src=${imagenes} class="imagen"/>
                             <h3 id= "nombre">  ${nombre}</h3>
                             <b>$${precio}</b>
                             <p>Disponible: ${stock} unidades</p>
-                            <button id="boton${id}" type="button" class= "btn btn-outline-dark" onclick="addToCart(${precio},${id},${stock})"> COMPRAR </button>
+                            <button id="boton${id}" type="button" class= "btn btn-outline-dark" onclick="card(${precio},${id},${stock})"> COMPRAR </button>
                             </div>`
         ArticulosPantalla.appendChild(contenedor);
     }
@@ -222,23 +108,25 @@ function renderProductos(categoria) {
 
 /* Funcion Agregar carrito */
 
-function addToCart(precio, id, stock) {
+function card(precio, id, stock) {
     for (const producto of listaProductos) {
         if (producto.id === id) {
             if (stock > 0) {
                 carrito.push(precio);
                 producto.venta(1);
-
                 precioTotalVenta = carrito.reduce((partialSum, a) => partialSum + a, 0);
                 PrecioTotal();
                 mostrarProductos();
-                alertAgregado("Agregaste '" + Articulo.nombre + "' a tu carrito");
+                alertAgregado("Agregaste '" + producto.nombre + "' a tu carrito");
             } else {
-                stockInsuficiente(Articulo);
+                stockInsuficiente(producto);
             }
         }
     }
 }
+
+
+
 
 /* Funcion Stock Insuficiente */
 
@@ -266,16 +154,15 @@ function PrecioTotal() {
 
 function clearStorage() {
     localStorage.clear();
-    precioTotalVenta = carrito.reduce((partialRest, ) => - partialRest, 0);
-  
-    /* Agregado Sweeet */
+    precioTotalVenta = carrito.reduce((partialRest,) => - partialRest, 0);
+    /*  Sweeet */
     Swal.fire({
         title: 'ESTAS POR VACIAR CARRITO',
         text: 'Aceptar para finalizar',
         icon: 'error',
         confirmButtonText: 'Aceptar'
     })
-    PrecioTotal(); 
+    PrecioTotal();
 }
 
 function vaciarCarro() {
@@ -284,26 +171,21 @@ function vaciarCarro() {
     vaciador.innerHTML = `<button id="vaciadorCarrito" type="button" class="btn btn-primary" onclick="clearStorage()">Vaciar carrito</button>`;
 }
 
-/* Por los momento para vaciar carrito tengo que clickear 2 veces, pero al agregar otro articulo aparece la sumatoria anterior */
 
 
-/* Agregado Toastify */
+/* Toastify */
 function alertAgregado(text) {
-	Toastify({
-		text,
-		duration: 3000,
-		position: "right",
-		style: {
-			background: "linear-gradient(to right, #00ff, #000)"
-		}
-	}).showToast();
+    Toastify({
+        text,
+        duration: 3000,
+        position: "right",
+        style: {
+            background: "linear-gradient(to right, #00ff, #000)"
+        }
+    }).showToast();
 }
-
-
-
 
 mostrarProductos();
 PrecioTotal();
 vaciarCarro();
 console.log(carrito);
-
